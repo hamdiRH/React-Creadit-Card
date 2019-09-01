585a20a83951907b4b35fae9622548a1ed960f4a
hamdiRH <hamdirahal@gmail.com>
2019-09-01 12:29:25 +0100

    update

--- src/index.js
+++ src/index.js
@@ -15,16 +15,47 @@ class Card extends React.Component {
     };
   }
 
+  renderholername = name => {
+    if (!isNaN(name)) return 'Error'
+    return name.toUpperCase()
+  
+  }
+  rendercodenumber = number => {
+    if (isNaN(number)) return 'Error'
+    return number
+  }
+  renderCardNumber = number => {
+    if (isNaN(number)) {
+      return 'Error';
+    }
+    number = number.toString();
+    let resultStr = "";
+    for (let i = 0; i < number.length; i += 4) {
+      resultStr += number.slice(i, i + 4) + " ";
+    }
+    if (resultStr === "") { resultStr = "0000 0000 0000 0000" }
+    return resultStr.trim();
+  };
+  
+  renderValidThru = number => {
+    if (isNaN(number)) {
+      return 'Error';
+    }
+    number = number.toString();
+    let test = number[0] + number[1]
+    if (test > 12) return 'Error'
+    let test2 = number[2] + number[3]
+    if (test2 < 19) return 'Error'
+  
+    if (number.length === 0) return ''
+    return number.slice(0, 2) + "/" + number.slice(2);
+  };
+  
+  
   render() {
     return (
       <div>
-        <CreditCard
-          creditcardTitle={this.state.creditcardTitle}
-          creditcardNumber={this.state.creditcardNumber}
-          creditcardHoldName={this.state.creditcardHoldName}
-          creditcardValidThru={this.state.creditcardValidThru}
-          creditcardValidCode={this.state.creditcardValidCode}
-        />
+        <CreditCard {...this.state} />
         <form style={{ marginTop: "50px" }}>
           <input
             maxLength="16"
@@ -32,7 +63,7 @@ class Card extends React.Component {
             placeholder="Card Number"
             onChange={event => {
               this.setState({
-                creditcardNumber: renderCardNumber(event.target.value)
+                creditcardNumber: this.renderCardNumber(event.target.value)
               });
             }}
           />
@@ -42,17 +73,17 @@ class Card extends React.Component {
             placeholder="Card Code"
             onChange={event => {
               this.setState({
-                creditcardValidCode: rendercodenumber(event.target.value)
+                creditcardValidCode: this.rendercodenumber(event.target.value)
               });
             }}
           />
           <input
             type="text"
-            
+
             placeholder="Holder Name"
             onChange={event => {
               this.setState({
-                creditcardHoldName: renderholername(event.target.value)
+                creditcardHoldName: this.renderholername(event.target.value)
               });
             }}
           />
@@ -62,8 +93,8 @@ class Card extends React.Component {
             placeholder="Valid thru"
             onChange={event => {
               this.setState({
-                creditcardValidThru: renderValidThru(event.target.value)
-               
+                creditcardValidThru: this.renderValidThru(event.target.value)
+
               });
             }}
           />
@@ -73,44 +104,5 @@ class Card extends React.Component {
   }
 }
 
-const renderholername = name => {
-  if (!isNaN(name)) return 'Error'
-  return name.toUpperCase()
-
-}
-const rendercodenumber = number =>{
-  if (isNaN(number)) return 'Error'
-  return number
-}
-const renderCardNumber = number => {
-  if (isNaN(number)) {
-    return 'Error';
-  }
-  number = number.toString();
-  let resultStr = "";
-  for (let i = 0; i < number.length; i += 4) {
-    resultStr += number.slice(i, i + 4) + " ";
-  }
-  if(resultStr === ""){resultStr="0000 0000 0000 0000"} 
-  return resultStr.trim();
-};
-
-const renderValidThru = number => {
-  if (isNaN(number)) {
-    return 'Error';
-  }
-  number = number.toString();
-  let test=number[0]+number[1]
-  if(test>12) return 'Error'
-  let test2=number[2]+number[3]
-  if(test2<19) return 'Error'
-
-  if(number.length===0) return ''
-  return number.slice(0, 2) + "/" + number.slice(2);
-};
-
-let Inputform = () => {
-  return <div />;
-};
 
 ReactDOM.render(<Card />, document.getElementById("root"));
